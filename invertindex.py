acb4e30554ef164ee31da44d6f2358b8296c605c
Brett Kaplan <btkaplan@uci.edu>
2023-02-17 14:17:49 -0800

    parsed, tokenized, and stemmed words in json content

--- invertindex.py
+++ invertindex.py
@@ -1,34 +1,44 @@
-import json, os, re
+import json, os
 from bs4 import BeautifulSoup
 import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
-os.chdir(os.getcwd() + "/DEV")
-print(f"MAIN DIRECTORY: {os.getcwd()}")
+stemmer = PorterStemmer()
 
-inverted_index = dict()
+def get_index(path: str): #  -> dict(str, tuple(int, list))
+    inverted_index = dict()
+    x = 1
+    for domain in os.listdir(os.getcwd()):
+      while x < 2: # while loop to get the for-loop to run only once for testing
+        try:
+          x += 1
+          print(f"DOMAIN: {domain}")
+          os.chdir(domain)
+          for file in os.listdir(os.getcwd()):
+            with open(file, 'r') as f:
+                #gets raw_content from json and gets a string containing all text content
+                raw_content = json.load(f)["content"]
+                soup = BeautifulSoup(raw_content, features="xml")
+                p_tags = soup.find_all('p')
+                text = ""
+                for p_tag in p_tags:
+                  text += p_tag.get_text().replace("\n\n", "")
 
-x = 1
-for domain in os.listdir(os.getcwd()):
-  while x < 2: # while loop to get the for-loop to run only once for testing
-    try:
-      x += 1
-      print(f"DOMAIN: {domain}")
-      os.chdir(domain)
-      for file in os.listdir(os.getcwd()):
-        with open(file, 'r') as f:
-            content = json.load(f)["content"]
-            soup = BeautifulSoup(content, features="xml")
-            # pattern = r'var elementorFrontendConfig\s*=\s*({.*});'
-            # script_tag = soup.find('script', string=lambda text: text and "elementorFrontendConfig" in text)
-            # script_tag.string = re.sub(pattern, '', script_tag.string)
-            # soup.prettify()
-            print(soup.get_text())
-            
-            # NEXT STEPS
-            # tokenize (including stemming) soup.gettext()
-            # create dictionary with reference to docIDs
+                #tokenizes and steams all words in text
+                tokens = word_tokenize(text)
+                stemmed_words = [stemmer.stem(token) for token in tokens if token.isalnum()]
+                print(stemmed_words)
 
-      print("----------\n")
-      os.chdir("..")
-    except Exception as e:
-      print(e)
+                # NEXT STEPS
+                # create dictionary with reference to docIDs
+                # --dictionary format: {key: token, value: (frequency, [docA, docB, docC...for all documents token appears in])}
+
+          print("----------\n")
+          os.chdir("..")
+        except Exception as e:
+          print(e) #this will likely be hit when the for-loop hits hidden non-directories in the /DEV folder when iterating
+    return inverted_index # returns empty dictionary for now
+
+if __name__ == "__main__":
+  get_index(os.chdir(os.getcwd() + "/DEV"))
