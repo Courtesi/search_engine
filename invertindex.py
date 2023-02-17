44c2375b40eef342a30369ac36123f707504b933
Brett Kaplan <btkaplan@uci.edu>
2023-02-17 01:00:28 -0800

    loops through all folders and lists all json files

--- invertindex.py
+++ invertindex.py
@@ -1,4 +1,11 @@
-if __name__ == "__main__":
-  print("Hello World")
+import json, os
 
-  #brett testing push
+os.chdir(os.getcwd() + "/DEV")
+print(os.getcwd())
+for domain in os.listdir(os.getcwd()):
+  print(domain)
+  os.chdir(domain)
+  for page in os.listdir(os.getcwd()):
+    print(page)
+  print("----------\n")
+  os.chdir("..")
