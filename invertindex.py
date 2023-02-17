1b1162ba0fe486ad38e7629726af8bb1c332196d
Brett Kaplan <btkaplan@uci.edu>
2023-02-17 02:22:44 -0800

    stuck parsing file body text content correctly w/ beautiful soup

--- invertindex.py
+++ invertindex.py
@@ -1,13 +1,34 @@
-import json, os
+import json, os, re
+from bs4 import BeautifulSoup
+import nltk
 
 os.chdir(os.getcwd() + "/DEV")
-print(os.getcwd())
+print(f"MAIN DIRECTORY: {os.getcwd()}")
+
+inverted_index = dict()
+
+x = 1
 for domain in os.listdir(os.getcwd()):
-  try:
-    print(domain)
-    os.chdir(domain)
-    print(f"{len(os.listdir(os.getcwd()))} files")
-    print("----------\n")
-    os.chdir("..")
-  except:
-    print(f"Skipped non-directory: {domain}")
+  while x < 2: # while loop to get the for-loop to run only once for testing
+    try:
+      x += 1
+      print(f"DOMAIN: {domain}")
+      os.chdir(domain)
+      for file in os.listdir(os.getcwd()):
+        with open(file, 'r') as f:
+            content = json.load(f)["content"]
+            soup = BeautifulSoup(content, features="xml")
+            # pattern = r'var elementorFrontendConfig\s*=\s*({.*});'
+            # script_tag = soup.find('script', string=lambda text: text and "elementorFrontendConfig" in text)
+            # script_tag.string = re.sub(pattern, '', script_tag.string)
+            # soup.prettify()
+            print(soup.get_text())
+            
+            # NEXT STEPS
+            # tokenize (including stemming) soup.gettext()
+            # create dictionary with reference to docIDs
+
+      print("----------\n")
+      os.chdir("..")
+    except Exception as e:
+      print(e)
