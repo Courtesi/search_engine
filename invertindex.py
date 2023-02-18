ec45087a1c875d2703be754a89bc3fe1bf8355a5
Brett Kaplan <btkaplan@uci.edu>
2023-02-17 16:35:44 -0800

    basic dictionary without efficient docIDs nor frequencies

--- invertindex.py
+++ invertindex.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+from collections import defaultdict
 
 stemmer = PorterStemmer()
 
 def get_index(path: str): #  -> dict(str, tuple(int, list))
-    inverted_index = dict()
+    inverted_index = defaultdict(str)
     x = 1
     for domain in os.listdir(os.getcwd()):
       while x < 2: # while loop to get the for-loop to run only once for testing
@@ -18,20 +19,27 @@ def get_index(path: str): #  -> dict(str, tuple(int, list))
           for file in os.listdir(os.getcwd()):
             with open(file, 'r') as f:
                 #gets raw_content from json and gets a string containing all text content
-                raw_content = json.load(f)["content"]
+                all_content = json.load(f)
+                raw_content = all_content["content"]
                 soup = BeautifulSoup(raw_content, features="xml")
                 p_tags = soup.find_all('p')
                 text = ""
                 for p_tag in p_tags:
                   text += p_tag.get_text().replace("\n\n", "")
 
-                #tokenizes and steams all words in text
+                #tokenizes and stems all words in text
                 tokens = word_tokenize(text)
                 stemmed_words = [stemmer.stem(token) for token in tokens if token.isalnum()]
-                print(stemmed_words)
+  
 
                 # NEXT STEPS
                 # create dictionary with reference to docIDs
+
+                for token in tokens:
+                  inverted_index[token] += (all_content["url"][:12] + "...") #shortening url for simple testing atm
+
+                print(inverted_index)
+
                 # --dictionary format: {key: token, value: (frequency, [docA, docB, docC...for all documents token appears in])}
 
           print("----------\n")
