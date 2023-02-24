e819b3e677af131cc21aaaca4a968cab854a6189
Brett Kaplan <74386612+BrettTK@users.noreply.github.com>
2023-02-24 00:46:42 -0800

    m2
    run for the first time to create index json file, then comment out line 87 and just use the search feature.

--- invertindex.py
+++ invertindex.py
@@ -1,64 +1,91 @@
-import json, os, pickle, sys
+import json
+import math
+import os
+import pickle
+import sys
+import requests
+import store_index
+from collections import Counter
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
-
 stemmer = PorterStemmer()
 
-def get_index(path: str): #  -> dict(str, tuple(int, list))
-    inverted_index = dict()
-    docCount = 0
-    for domain in os.listdir(path):
-      try:
-        print(f"DOMAIN: {domain}")
-        # if this following line runs without raising an exception, the directory is valid
-        os.chdir(domain)
-        for file in os.listdir(os.getcwd()):
-          docCount += 1
-          with open(file, 'r') as f:
-              #gets raw_content from json and gets a string containing all text content
-              all_content = json.load(f)
-              raw_content = all_content["content"]
-              docID = hash(all_content["url"])
-              soup = BeautifulSoup(raw_content, features="xml")
-              p_tags = soup.find_all('p')
-              text = ""
-              for p_tag in p_tags:
-                text += p_tag.get_text().replace("\n\n", "")
+def process_file(file_path):
+    if file_path.startswith('http'):
+        response = requests.get(file_path)
+        raw_content = response.text
+        docID = file_path
+    else:
+        with open(file_path, 'r') as f:
+            all_content = json.load(f)
+            raw_content = all_content["content"]
+            docID = all_content["url"]
+    soup = BeautifulSoup(raw_content, features="lxml")
+    p_tags = soup.find_all('p')
+    text = ""
+    for p_tag in p_tags:
+        text += p_tag.get_text().replace("\n\n", "")
+    tokens = word_tokenize(text)
+    tokens = [stemmer.stem(token).lower() for token in tokens if token.isalnum()]
+    return (docID, Counter(tokens))
 
-              #tokenizes and stems all words in text
-              tokens = word_tokenize(text)
-              tokens = [stemmer.stem(token).lower() for token in tokens if token.isalnum()]
-# --dictionary format: {token -> [[docA, freqA], [docB, freqB],...for all tokens in all documents])}
-              # for every token...
-              for token in tokens:
-                # and every index pair,
-                for item in inverted_index.items():
-                  # if the token already exists...
-                  if token in item[0]:
-                    # and it was already found in this specific document,
-                    if item[1][0] == docID:
-                      # increase the frequency by one
-                      inverted_index[item[0]][1] += 1
-                    # or it was found in a previous document but it is appearing first in this document,
-                    elif item[1][0] != docID:
-                      # add the docID to the token value and give it an initial frequency of 1
-                      inverted_index[item[0]].append([docID, 1])
-                # otherwise, if the token does not exist, create a new index entry with the token --> (docID, 1)
-                else:
-                  inverted_index[token] = [docID, 1]
-        print(inverted_index)
-        print("-------------------------------\n")
-        os.chdir("..")
-      except Exception as e:
-        #this will likely be hit when the for-loop hits hidden non-directories in the /DEV folder when iterating
-        pass
+def get_index(path: str):
+    inverted_index = {}
+    docCount = 0
+    pool = Pool()
+    for domain in os.listdir(path):
+        try:
+            print(f"DOMAIN: {domain}")
+            os.chdir(os.path.join(path, domain))
+            file_paths = [os.path.join(os.getcwd(), file) for file in os.listdir(os.getcwd())]
+            results = pool.map(process_file, file_paths)
+            for result in results:
+                docID = result[0]
+                token_counts = result[1]
+                for token, count in token_counts.items():
+                    inverted_index.setdefault(token, []).append([docID, count])
+            docCount += len(results)
+            print("-------------------------------\n")
+            print(inverted_index)
+            os.chdir("..")
+        except Exception as e:
+            print(e)
     print(f"# OF DOCUMENTS: {docCount}")
     print(f"# OF UNIQUE WORDS {len(inverted_index)}")
     index_in_bytes = pickle.dumps(inverted_index)
     print(f"SIZE IN KILOBYTES: {round(sys.getsizeof(index_in_bytes)/1000, 2)}")
-    return inverted_index # returns empty dictionary for now
+    return inverted_index
+
+def findResults(qWords: list, index: dict):
+    with open(index, 'r') as f:
+        index = json.load(f)
+    #print("irvine" in index)
+
+    matched_docs = []
+    firstWord = True
+    qWords = [stemmer.stem(word).lower() for word in qWords]
+    for word in qWords:
+        current_docs = []
+        if word in index:
+            for key, data in index.items():
+                if word == key:
+                    for doc in data:
+                        if firstWord:
+                            matched_docs.append(doc)
+                        current_docs.append(doc)
+        matched_docs = [doc for doc in matched_docs if doc in current_docs]
+        firstWord = False
+    matched_docs.sort(key=lambda x: -1 * x[1])
+    for x in range(0,min(5, len(matched_docs))):
+        print(f"{x+1}) {matched_docs[x]}")
+    return matched_docs
 
 if __name__ == "__main__":
-  get_index(os.chdir(os.getcwd() + "/DEV"))
+    store_index.store(get_index(os.path.join(os.getcwd(), "DEV"))) #comment this out after the index file is created for the first time.
+                                                                    #it is saved in the DEV folder
+    query = input("SEARCH: ")
+    qWords = query.split()
+    findResults(qWords, "stored_index.json")
