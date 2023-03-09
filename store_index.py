1f06e20c61193667e246674e4311f47979355007
Brett Kaplan <74386612+BrettTK@users.noreply.github.com>
2023-03-09 02:53:31 -0800

    Changed format
    Easier to parse for creating byte index

--- store_index.py
+++ store_index.py
@@ -2,4 +2,5 @@ import json
 
 def store(index: dict):
     with open("stored_index.json", "w") as f:
-        json.dump(index, f)
+        json.dump(index, f, indent=0, separators=(", ", ": "))
+        f.write("\n")
