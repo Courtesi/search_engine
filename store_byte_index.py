72d8dd3b165f00406044bb424eedf0358e10f001
Brett Kaplan <74386612+BrettTK@users.noreply.github.com>
2023-03-10 01:29:07 -0800

    Adjusted for multiple indices
    Now returns which of the three indices the token is in along with its respective byte location

--- store_byte_index.py
+++ store_byte_index.py
@@ -4,13 +4,12 @@ import re
 def write_byte_index():
     map = {}
 
-    with open("stored_index.json", 'rb') as f:
-        for line in f:
-            # if line in json file starts with a quote a.k.a. it is a token
-            if line[0:1].decode('utf-8') == "\"":
-                map[re.sub(r'\W+', '', line.decode('utf-8'))] = f.tell() - len(line)
+    for i in range(1,4):
+        with open(f"partial_index{i}.json", 'rb') as f:
+            for line in f:
+                if line[0:1].decode('utf-8') == "\"":
+                    map[re.sub(r'\W+', '', line.decode('utf-8'))] = [i, f.tell() - len(line)]
     
-    print("RUN2")
     with open("stored_byte_index.json", "w") as f:
         json.dump(map, f, indent=0, separators=(", ", ": "))
 
